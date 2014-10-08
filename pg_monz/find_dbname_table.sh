777037fae9919cc533a0ec6de64d91cfd22a7f36
Kohei Takahashi <kotakahashi@aiming-inc.com>
2014-10-08 13:06:14 +0900

    First support for PostgreSQL 9.1
      Note: This change will break 9.2 (and later) support.
    
    Signed-off-by: Kohei Takahashi <kotakahashi@aiming-inc.com>

--- pg_monz/find_dbname_table.sh
+++ pg_monz/find_dbname_table.sh
@@ -23,23 +23,23 @@ GETDB="select datname from pg_database where datistemplate = 'f';"
 #
 # For pgbench tables, set the GETTABLE as 
 #GETTABLE="select \
-#            row_to_json(t) \
+#            concat_ws(',',database,schemaname,tablename) \
 #          from (
-#            select current_database() as "{#DBNAME}\",schemaname as \"{#SCHEMANAME}\",tablename as \"{#TABLENAME}\" \
+#            select current_database() as \"database\",schemaname,tablename \
 #            from \
 #              pg_tables \
 #            where \
 #              schemaname not in ('pg_catalog','information_schema') \
 #            and \
 #              tablename in ('pgbench_accounts','pgbench_branches','pgbench_history','pgbench_tellers') \
-#           ) as t" 
+#          ) as t"
  
-
-GETTABLE="select row_to_json(t) from (select current_database() as \"{#DBNAME}\",schemaname as \"{#SCHEMANAME}\",tablename as \"{#TABLENAME}\" from pg_tables where schemaname not in ('pg_catalog','information_schema')) as t"
+GETTABLE="select concat_ws(',',database,schemaname,tablename) from (select current_database() as \"database\",schemaname,tablename from pg_tables where schemaname not in ('pg_catalog','information_schema')) as t"
 
 for dbname in $(psql -h $1 -p $2 -U $3 -d $4 -t -c "${GETDB}"); do
     for tablename in $(psql -h $1 -p$2 -U $3 -d $dbname -t -c "${GETTABLE}"); do
-    dblist="$dblist,"$tablename
+        read db schema tbl < <(echo ${tablename//,/ })
+        dblist="$dblist,{\"{#DBNAME}\":\"$db\",\"{#SCHEMANAME}\":\"$schema\",\"{#TABLENAME}\":\"$tbl\"}"
     done
 done
 echo '{"data":['${dblist#,}' ]}'
